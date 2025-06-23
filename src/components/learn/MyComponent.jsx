42d6df568bb2e6ce8452d10283089e524d0a6ae8
thuannd2k5 <nguyenthuan22112005@gmail.com>
2025-06-23 16:40:41 +0700

    cách sử dụng biến số với JSX

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -2,9 +2,18 @@
 import './style.css';
 
 const MyComponent = () => {
+
+    const arr = [1, 2, 3, 4, 5];
+    const obj = {
+        name: 'thuan',
+        age: 18,
+    }
+    //chuyển obj sang kiểu string thì dùng json.stringify
+    //có thể viết code js trong html bằng cách sử dụng dấu ngoặc nhọn {}
     return (
         <>
-            <div>thuan & hoidanit</div>
+            <div>thuan & {arr[3]} & {JSON.stringify(obj)}</div>
+            <div>{console.log('Thuan dz')}</div>
             <div className='child'>lớp con</div>
         </>
     );
