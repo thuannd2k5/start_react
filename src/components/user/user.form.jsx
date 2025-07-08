cd726694d60e0dabf35077f75280a92b0c14ac3c
thuannd2k5 <nguyenthuan22112005@gmail.com>
2025-07-08 15:54:44 +0700

    state hoa form

---
+++ src/components/user/user.form.jsx
@@ -0,0 +1,58 @@
+import { Input } from 'antd';
+import { Button, Flex } from 'antd';
+import { useState } from 'react';
+
+const UserForm = () => {
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [phone, setPhone] = useState("");
+
+    const handClickBtn = () => {
+        console.log("check form data >>>", { fullName, email, password, phone });
+    }
+
+    return (
+        <div className='user-form' style={{ margin: "20px 0" }}>
+            <div style={{ display: "flex", gap: "15px", flexDirection: " column" }}>
+                <div>
+                    <span>FullName</span>
+                    <Input
+                        value={fullName}
+                        onChange={(event) => { setFullName(event.target.value) }}
+                    />
+                </div>
+                <div>
+                    <span>Email</span>
+                    <Input
+                        value={email}
+                        onChange={(event) => { setEmail(event.target.value) }}
+                    />
+                </div>
+                <div>
+                    <span>Password</span>
+                    <Input.Password
+                        value={password}
+                        onChange={(event) => { setPassword(event.target.value) }}
+                    />
+                </div>
+                <div>
+                    <span>Phone Number</span>
+                    <Input
+                        value={phone}
+                        onChange={(event) => { setPhone(event.target.value) }}
+                    />
+                </div>
+                <div>
+                    <Button
+                        onClick={handClickBtn}
+                        type='primary'>create user</Button>
+                </div>
+            </div>
+
+        </div>
+    )
+
+}
+
+export default UserForm;
