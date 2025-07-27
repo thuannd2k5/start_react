1e5ecf9fe396bbcff8856cc7ae5805c570659154
thuannd2k5 <nguyenthuan22112005@gmail.com>
2025-07-27 20:25:45 +0700

    bắt đầu sử dụng unctroller

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,7 +1,55 @@
-
+import { Button, Input, Form } from "antd";
 const RegisterPage = () => {
+    const [form] = Form.useForm();
+    const onFinish = (values) => {
+        console.log("check values", values);
+    }
     return (
-        <div>Register page</div>
+        <Form
+            form={form}
+            layout="vertical"
+            onFinish={onFinish}
+        // onFinishFailed={onFinishFailed}
+        // autoComplete="off"
+        >
+            <div style={
+                {
+                    margin: "50px"
+                }}>
+                <Form.Item
+                    label="Full Name"
+                    name="fullName"
+                // rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Email"
+                    name="email"
+                // rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Password"
+                    name="password"
+                // rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input.Password />
+                </Form.Item>
+                <Form.Item
+                    label="Phone Number"
+                    name="phone"
+                // rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input />
+                </Form.Item>
+                <Button
+                    onClick={() => form.submit()}
+                    type="primary"
+                >register</Button>
+            </div>
+        </Form>
     )
 }
 
